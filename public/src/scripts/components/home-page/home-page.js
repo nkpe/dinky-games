4326b188e03bcbc07854e9cedecb34989e94630a
nkpe <naomi.pearse@gmail.com>
2022-11-06 09:46:18 +0000

    home page content wrapper added

--- public/src/scripts/components/home-page/home-page.js
+++ public/src/scripts/components/home-page/home-page.js
@@ -1,14 +1,32 @@
 'use strict';
 
 import {main} from '../../index.js';
+const mazeImg = '/src/imgs/maze/maze-overview.png';
+const mazeCaption = 'Dinky Maze';
+
+const HomeContentWrapper = (caption, imgSrc, section) => {
+    const wrapper = document.createElement('div');
+    const figure = document.createElement('figure');
+    const img = document.createElement('img');
+    const figCaption = document.createElement('figcaption');
+
+    wrapper.className = 'home-wrapper';
+    img.src = imgSrc;
+    figCaption.innerText = caption;
+    figCaption.className = 'fig-caption';
+
+    section.appendChild(wrapper);
+    wrapper.appendChild(figure);
+    figure.appendChild(figCaption);
+    figure.appendChild(img);
+};
+
 
 const HomePage = () => {
     const section = document.createElement('section');
-    section.className = "home-page";
+    section.id = 'home-page';
     main.appendChild(section);
-    section.innerHTML = `
-    <h2>Home Page</h2>`
-
+    HomeContentWrapper(mazeCaption, mazeImg, section);
 }
 
 export default HomePage;
