ad2512467ef91895ad7ed47a887412a9f8ca59b2
nkpe <naomi.pearse@gmail.com>
2022-11-06 09:02:53 +0000

    homepage routing setup

--- public/src/scripts/index.js
+++ public/src/scripts/index.js
@@ -1,14 +1,9 @@
 'use strict';
 console.log("index.js working");
 
-
 import { mazeGame } from "./maze-game/maze.js";
 
-// User clicks - maze-game.js needs to run function, before setting the section to display: block. 
-
 const main = document.getElementById("main");
-const navMaze = document.getElementById("nav-maze");
-// const navSolitare = document.getElementById("nav-maze");
 
 const hideAllSections = () => {
     const allSections = document.getElementsByTagName("section");
@@ -17,18 +12,23 @@ const hideAllSections = () => {
     };
 };
 
-
-
-const navEvents = () => {
-
-    window.addEventListener('hashchange', () => {
+const hashRouting = () => {
         if (location.hash === "#maze"){
             hideAllSections();
             mazeGame(main);
         } else if (location.hash = "#home"){
             hideAllSections();
-        }       
-    });
+        };
+        console.log("navEvents working")
+    ;
 };
 
-navEvents();
+hashRouting();
+window.addEventListener('hashchange', hashRouting);
+// window.addEventListener('reload', hashRouting);
+window.addEventListener('load', () => {
+    if (location.hash === ''){
+        location.hash = "#home";
+    }
+    hashRouting();
+});
